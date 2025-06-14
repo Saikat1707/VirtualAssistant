9f8b5bc2a3dc8b65f57f8fbc20ab99b131c79c72
Saikat1707 <berasaikat731@gmal.com>
2025-06-14 10:38:37 +0530

    edit

--- Backend/index.js
+++ Backend/index.js
@@ -10,9 +10,7 @@ import { geminiResponse } from './src/service/GeminiRespose.js'
 
 const corsOptions = {
   origin: "http://localhost:5173",
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  credentials: true
 }
 
 const app = express()
