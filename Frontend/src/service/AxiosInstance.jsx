1cf95256741777be1e1db7f3c581d589d39fada3
Saikat1707 <berasaikat731@gmal.com>
2025-06-14 11:00:40 +0530

    edit

--- Frontend/src/service/AxiosInstance.jsx
+++ Frontend/src/service/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const asiosInstance = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://virtualassistant-t395.onrender.com/api",
     withCredentials: true,
 })
 
